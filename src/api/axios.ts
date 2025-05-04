3f3451186aa242cd04b86879b962dd619dab4e3f
devlee <amy000809@gmail.com>
2025-05-04 10:55:44 +0900

    🎯 Fix: baseURL이 undefined일 경우 예외처리 추가

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,11 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === 'production';
+const baseURL = isProduction ? process.env.NEXT_PUBLIC_API_URL : '/api';
+
+if (!baseURL) {
+  throw new Error('NEXT_PUBLIC_API_URL is not defined in production');
+}
+
 const axiosInstance = axios.create({
-  baseURL: isProduction ? process.env.NEXT_PUBLIC_API_URL : "/api",
+  baseURL,
   timeout: 10000,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
   //   withCredentials: true, // 쿠키 사용 시
 });
